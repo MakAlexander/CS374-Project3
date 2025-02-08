b92d992284899a6db80de733e84e70f3ce5de9bb
Alexander Mak <alexander.o.mak@gmail.com>
2025-02-08 01:28:47 -0500

    Initial project set up

---
+++ makal_assignment3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct movie
+{
+    char *title;
+    int year;
+    struct movie *next
+};
+
+// Menu Functions
+    // mainMenu()
+    // fileSelectionMenu()
+// File Handling Functions
+    // largestFile()
+    // smallestFile()
+    // byFileName()
+// Data Processing Functions
+    // processFile()
+    // createMovie()
+// Output Fuctions
+    // writeMoviesByYear()
