8d0b639b43fd4d42f8405ac6c9653ebbf3741c4f
Alexander Mak <alexander.o.mak@gmail.com>
2025-02-08 23:29:16 -0500

    main menu functionality

--- makal_assignment3.c
+++ makal_assignment3.c
@@ -6,12 +6,40 @@ struct movie
 {
     char *title;
     int year;
-    struct movie *next
+    struct movie *next;
+};
+
+void mainMenu();
+
+int main() {
+    mainMenu();
+    return 0;
+
 };
 
 // Menu Functions
-    // mainMenu()
-    // fileSelectionMenu()
+void mainMenu() {
+    int choice;
+
+    do {
+        printf("1. Select file to process\n2. Exit the program\nEnter a choice (1 or 2): ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                //fileSelectionMenu();
+                break;
+            case 2:
+                printf("Exiting the program.\n");
+                break;
+            default:
+                fprintf(stderr, "You entered an incorrect choice. Try again.\n");
+        }
+    } while (choice != 2);  // Loop continues unless user chooses to exit
+
+}
+
+// fileSelectionMenu()
 // File Handling Functions
     // largestFile()
     // smallestFile()
